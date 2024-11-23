22e56968741e93cf0694c05e9156be958deb2b68
ivantarabanovych <vanjokcitet334@gmail.com>
2024-11-23 22:01:44 +0200

    q2

--- dist/main.js
+++ dist/main.js
@@ -8,7 +8,16 @@ class Car extends Vehicle {
     startEngine() {
         return "Car engine started";
     }
+    drive() {
+        console.log("Car is driving");
+    }
+}
+class Bicycle {
+    drive() {
+        console.log("Bicycle is being ridden");
+    }
 }
 const myCar = new Car();
-console.log(myCar.startEngine());
-console.log(myCar.move());
+const myBicycle = new Bicycle();
+myCar.drive();
+myBicycle.drive();
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
+interface Drivable {
+    drive(): void;
+}
+
 abstract class Vehicle {
-    abstract startEngine(): string;
-    move(): string {
+    abstract startEngine(): string
+    
+    move(): string{
         return "Vehicle is moving"
     }
 }
 
-class Car extends Vehicle{
+class Car extends Vehicle implements Drivable{
     startEngine(): string {
         return "Car engine started"
+    }
+
+    drive(): void {
+        console.log("Car is driving");
+    }
+}
+
+class Bicycle implements Drivable{
+    drive(): void {
+        console.log("Bicycle is being ridden");
     }
 }
 
 const myCar = new Car();
-console.log(myCar.startEngine());
-console.log(myCar.move());
+const myBicycle = new Bicycle();
+
+myCar.drive();
+myBicycle.drive();
